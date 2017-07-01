4361cf185389848955cee27d5f75188dc4747ae4
Zeb Burke-Conte <zmbc@uw.edu>
2017-07-01 12:57:36 -0700

    Update to elastic IP

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -6,7 +6,7 @@
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
-server '54.218.13.200', user: 'deploy', roles: %w[web app db]
+server '34.210.15.22', user: 'deploy', roles: %w[web app db]
 
 # role-based syntax
 # ==================
